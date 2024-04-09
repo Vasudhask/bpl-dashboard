5a9a4910142d97b0acb07541d7e0f1e0b62072ac
Vasudha S K <90177051+Vasudhask@users.noreply.github.com>
2024-04-09 22:45:34 +0530

    Update my_dashboard.py

--- my_dashboard.py
+++ my_dashboard.py
@@ -150,7 +150,7 @@ def function_performance():
          return spec_conf_names
 
     # Convert the 'months' column to datetime format
-    #df['date'] = pd.to_datetime(df['date'], format='%b-%y')
+    df['date'] = pd.to_datetime(df['date'], format='%b-%y')
 
     def moving_average_all(df, n):
          result = {}
@@ -169,7 +169,7 @@ def function_performance():
 
 
     # Convert the 'months' column to datetime format
-    #df['date'] = pd.to_datetime(df['date'], format='%b-%y')
+    df['date'] = pd.to_datetime(df['date'], format='%b-%y')
 
     def moving_average_specific(df, selected_category, n):
          result = {}
